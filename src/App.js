7c308daa610e62bb36a51c1e98ac32d1d5919d6a
Kaushikselvaraju <kaushikselvaraju7@gmail.com>
2021-12-25 15:24:30 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import NewsFeed from './components/NewsFeed'
+import CurrencyConverter from './components/CurrencyConverter'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+        <h1>Crypto Dashboard</h1>
+        <div className="app-wrapper">
+            <CurrencyConverter />
+            <NewsFeed />
+        </div>
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
