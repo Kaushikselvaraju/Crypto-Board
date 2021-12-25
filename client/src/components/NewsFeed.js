dc2e8b925f425a8cbb4105449c2b26c54da78ea9
HariKrishna-28 <harikrishnac2002@gmail.com>
2021-12-25 18:04:52 +0530

    stable working build

--- client/src/components/NewsFeed.js
+++ client/src/components/NewsFeed.js
@@ -1,4 +1,4 @@
-import {useEffect, useState} from 'react'
+import { useEffect, useState } from 'react'
 import axios from 'axios'
 
 const NewsFeed = () => {
@@ -8,7 +8,7 @@ const NewsFeed = () => {
 
         const options = {
             method: 'GET',
-            url: 'http://localhost:8000/news'
+            url: 'http://localhost:5000/news'
         }
 
         axios.request(options).then((response) => {
@@ -22,14 +22,14 @@ const NewsFeed = () => {
 
     console.log(articles)
 
-    const first7Articles = articles?.slice(0,7)
+    // const first7Articles = articles?.slice(0, 7)
 
     return (
         <div className="news-feed">
             <h2>News Feed</h2>
-            {first7Articles?.map((article, _index) => (
+            {articles?.map((article, _index) => (
                 <div key={_index}>
-                    <a href={article.url}><p>{article.title}</p></a>
+                    <a href={article.url} target="_blank" rel="noreferrer"><p>{article.title}</p></a>
                 </div>))}
         </div>
     )
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const PORT = 8000
+// const PORT = 8000
 const express = require('express')
 const cors = require('cors')
 const axios = require('axios')
@@ -8,22 +8,23 @@ const app = express()
 
 app.use(cors())
 
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
     res.json('hi')
 })
 
-app.get('/convert', (req,res) => {
+app.get('/convert', (req, res) => {
     const toCurrency = req.query.to_currency
     const fromCurrency = req.query.from_currency
+    // console.log(toCurrency, fromCurrency)
 
 
     const options = {
         method: 'GET',
         url: 'https://alpha-vantage.p.rapidapi.com/query',
-        params: {from_currency: chosenPrimaryCurrency, function: 'CURRENCY_EXCHANGE_RATE', to_currency: chosenSecondaryCurrency},
+        params: { from_currency: fromCurrency, function: 'CURRENCY_EXCHANGE_RATE', to_currency: toCurrency },
         headers: {
             'x-rapidapi-host': 'alpha-vantage.p.rapidapi.com',
-            'x-rapidapi-key': process.env.REACT_APP_RAPID_API_KEY
+            'x-rapidapi-key': 'f10962b933msh967b88ae830ffbfp1c3634jsn29aae1a8d962'
         }
     }
 
@@ -34,18 +35,19 @@ app.get('/convert', (req,res) => {
     })
 })
 
-app.get('/news', (req,res) => {
+app.get('/news', (req, res) => {
     const options = {
         method: 'GET',
         url: 'https://crypto-news-live.p.rapidapi.com/news',
         headers: {
             'x-rapidapi-host': 'crypto-news-live.p.rapidapi.com',
-            'x-rapidapi-key': process.env.REACT_APP_RAPID_API_KEY
+            'x-rapidapi-key': 'f10962b933msh967b88ae830ffbfp1c3634jsn29aae1a8d962'
         }
     }
 
     axios.request(options).then((response) => {
-        res.json(response.data)
+        res.json(response.data.slice(0, 5))
+        // console.log(response.data)
 
     }).catch((error) => {
         console.error(error)
@@ -53,4 +55,4 @@ app.get('/news', (req,res) => {
 })
 
 
-app.listen(8000, () => console.log(`Server is running on port ${PORT}`))
+app.listen(process.env.PORT || 5000, () => console.log(`Server is running `))
