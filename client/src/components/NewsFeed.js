ee27a8586af3b062abe3254e8e0daf3bfa5db2af
HariKrishna-28 <harikrishnac2002@gmail.com>
2021-12-27 15:42:34 +0530

    ui improvements

--- client/src/components/NewsFeed.js
+++ client/src/components/NewsFeed.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios'
-import { ScaleLoader } from 'load-animations-react'
+import { RippleLoader } from 'load-animations-react'
 const baseUrl = "https://crypto0dash.herokuapp.com"
 // const baseUrl = "http://localhost:5000"
 
@@ -43,9 +43,9 @@ const NewsFeed = () => {
                         alignItems: "center",
                         justifyContent: "center",
                         height: "50%",
-                        width: "300px",
+                        // width: "300px",
                     }}>
-                        <ScaleLoader />
+                        <RippleLoader />
                     </div>
                 )
             }
